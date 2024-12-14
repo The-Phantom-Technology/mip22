21baf4592dc1321bc826e07eb6181bc08ca2b683
The Phantom Technology -Advanced Pentest <155259033+The-Phantom-Technology@users.noreply.github.com>
2024-12-14 11:06:36 +0800

    更新

---
+++ PHOSBUILD.sh
@@ -0,0 +1,21 @@
+rm -rf /opt/PHANTX/mip22
+
+cd /opt/PHANTX
+
+git clone https://github.com/makdosx/mip22.git
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+cd /opt/mip22
+
+bash mip22.sh
+
+chmod -R 755 /opt/PHANTX
+rm -rf /opt/PHOS/tool/mip22
+rm -rf /opt/PHOS/tool/mip22
